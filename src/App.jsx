a79a8ffa8cf1fc01f4e90372076d7fc8a37f07d6
Ali Zadil <tarikz94@gmail.com>
2023-07-10 12:15:07 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
+import { useState, useRef } from "react";
 import "./App.css";
 
+const API_URL = "http://localhost:3001/api/items/"
+
 function App() {
-  return <div className="main-content"></div>;
+  const inputRef = useRef();
+  const [dataList, setDataList] = useState([]);
+
+  return (
+    <div className="main-content">
+      <input ref = {inputRef} type ="text" />
+      <button
+        onClick = {async () => {
+          let text = inputRef.current.value;
+          let response = await fetch(API_URL, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              value: text,
+            }),
+        });
+        let responseContent = await response.json();
+        setDataList(responseContent.list);
+        }}
+      >
+        Save
+      </button>
+      <div>
+        {dataList.map((el, index) => (
+          <div>
+            <p>{el.content}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,9 +1,22 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 3001;
 
 app.use(express.json());
+app.use(cors());
+
+var itemsList = [];
+
+app.post("/api/items", (request, response) => {
+  const value = request.body.value;
+  itemsList.push({
+    id: 0,
+    content: value,
+  });
+  response.json({ list: itemsList });
+});
 
 app.listen(port, () => {
   console.log("Server started on port", port);
